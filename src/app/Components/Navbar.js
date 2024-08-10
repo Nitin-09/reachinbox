bcd1843b25542b9e12b272bfda993fff20a73bf8
Nitin Gangwani <97083939+Nitin-09@users.noreply.github.com>
2024-08-10 18:53:26 +0530

    Update Navbar.js

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -37,8 +37,8 @@ function Navbar() {
           className='flex border-2 w-fit bg-[#E9EAEC] dark:bg-[#222426] border-[#DADEE1] dark:border-[#343A40] gap-[7px] px-[5px] py-[3px] rounded-[80px] cursor-pointer'
           onClick={toggleTheme}
         >
-          <Image width={20} height={20} src='\moon.svg'></Image>
-          <Image width={20} height={20} src='\sun.svg'></Image>
+          <Image width={20} height={20} alt='' src='\moon.svg'></Image>
+          <Image width={20} height={20} alt='' src='\sun.svg'></Image>
         </div>
         <div className='flex gap-2 justify-center items-center'>
           <span className='font-sans font-semibold text-sm text-[#454F5B] dark:text-white'>Tim's Workspace</span>
